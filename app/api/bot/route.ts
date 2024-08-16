c4ad5e8b8563da8499b919c0a1a01470f903a21a
Нуриддин Худойбердиев <nuriddin0495@gmail.com>
2024-08-16 19:31:58 +0500

    ffix chat idd save

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -12,35 +12,37 @@ if (!token) throw new Error('TELEGRAM_BOT_TOKEN environment variable not found.'
 
 const bot = new Bot(token);
 bot.command('start', async (ctx) => {
-	const message = ctx.message;
-	await dbConnect();
-	const findUser = await UserModel.findOne({ user_id: ctx.chat.id });
-	if (!findUser) {
-		if (message) {
-			if (message.text.includes('fren')) {
-				const parent_id = message.text.split('=')[1];
-				const user = await UserModel.create({
-					parent_id,
-					username: ctx.chat.username,
-					first_name: ctx.chat.first_name,
-					last_name: ctx.chat.last_name,
-					chat_id: ctx.chat.id,
-				});
-				user.save();
-			}
-		}
-		const user = await UserModel.create({
-			username: ctx.chat.username,
-			first_name: ctx.chat.first_name,
-			last_name: ctx.chat.last_name,
-		});
-		user.save();
-	}
+  const message = ctx.message;
+  await dbConnect();
 
-	ctx.reply(JSON.stringify(ctx.message?.text));
+  const findUser = await UserModel.findOne({ user_id: ctx.chat.id });
+  if (!findUser) {
+    if (message) {
+      if (message.text.includes('fren')) {
+        const parent_id = message.text.split('=')[1];
+        const user = await UserModel.create({
+          parent_id,
+          username: ctx.chat.username,
+          first_name: ctx.chat.first_name,
+          last_name: ctx.chat.last_name,
+          chat_id: ctx.chat.id,
+        });
+        user.save();
+      }
+    }
+    const user = await UserModel.create({
+      username: ctx.chat.username,
+      first_name: ctx.chat.first_name,
+      last_name: ctx.chat.last_name,
+      chat_id: ctx.chat.id,
+    });
+    user.save();
+  }
+
+  ctx.reply(JSON.stringify(ctx.message?.text));
 });
 bot.on('message:text', async (ctx) => {
-	await ctx.reply(ctx.message.text);
+  await ctx.reply(ctx.message.text);
 });
 
 export const POST = webhookCallback(bot, 'std/http');
