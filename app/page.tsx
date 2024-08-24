b16c9b94df80ff41d8da1310ab4ac85a22b72284
Нуриддин Худойбердиев <nuriddin0495@gmail.com>
2024-08-24 14:54:26 +0500

    ffix

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Game from './components/Game';
 import { useViewport } from '@tma.js/sdk-react';
 
 export default function Home() {
-  const view = useViewport();
-  useEffect(() => {
-    view?.expand();
-  }, [view]);
+  // const view = useViewport();
+  // useEffect(() => {
+  //   view?.expand();
+  // }, [view]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between m-8">
       <Game />
