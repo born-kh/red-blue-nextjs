65fa3d3ec73a094053621f92d126e00c93168b9f
Нуриддин Худойбердиев <nuriddin0495@gmail.com>
2024-08-25 08:32:19 +0500

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Game from './components/Game';
 import { useViewport } from '@tma.js/sdk-react';
+import SplashScreen from './components/SplashScreen';
 
 export default function Home() {
   const view = useViewport();
+  const [loading, setLoading] = useState(true);
+
+  const finishLoading = () => {
+    setLoading(false);
+  };
   useEffect(() => {
     view?.expand();
   }, [view]);
-  return (
+  return loading ? (
+    <SplashScreen finishLoading={finishLoading} />
+  ) : (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <Game />
     </main>
