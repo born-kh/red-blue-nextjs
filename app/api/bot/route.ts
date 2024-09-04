71761b28d78b11c238c8602d723b136963a76370
Нуриддин Худойбердиев <nuriddin0495@gmail.com>
2024-09-04 16:31:53 +0500

    fix

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -14,16 +14,14 @@ const bot = new Bot(token);
 bot.command('start', async (ctx) => {
   const message = ctx.message;
   await dbConnect();
-  console.log('match', ctx.match);
+
   const findUser = await UserModel.findOne({ user_id: ctx.chat.id });
 
-  console.log('findUser', message, ctx);
   if (!findUser) {
-    console.log('message', message);
     if (message) {
       if (message.text.includes('friend')) {
         const id = message.text.split('=')[1];
-        console.log('friend', id);
+
         const data: any = {
           username: ctx.chat.username,
           first_name: ctx.chat.first_name,
@@ -33,15 +31,14 @@ bot.command('start', async (ctx) => {
         };
 
         if (message?.text?.includes('fromApp')) {
-          data.parent_id = id;
-        } else {
           data.app_parent_id = id;
+        } else {
+          data.parent_id = id;
         }
         const user = await UserModel.create(data);
         user.save();
       } else if (message.text.includes('app_user_id')) {
         const id = message.text.split('=')[1];
-        console.log('app_user_id', id);
         const user = await UserModel.create({
           app_user_id: id,
           username: ctx.chat.username,
@@ -58,11 +55,6 @@ bot.command('start', async (ctx) => {
   ctx.reply(JSON.stringify(ctx.message?.text));
 });
 
-bot.command('add', async (ctx) => {
-  // `item` will be "apple pie" if a user sends "/add apple pie".
-  const item = ctx.match;
-  console.log('addd', ctx);
-});
 bot.on('message:text', async (ctx) => {
   await ctx.reply(ctx.message.text);
 });
