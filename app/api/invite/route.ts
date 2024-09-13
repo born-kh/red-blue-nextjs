5b68699fd3882098fc60ad0959b592dbc7442465
Nuriddin Khudoyberdiev <nkhudoiberdiev@tamos.tj>
2024-09-13 09:21:40 +0500

    changes api

--- app/api/invite/route.ts
+++ app/api/invite/route.ts
@@ -5,41 +5,41 @@ import UserModel from '@/app/model/userModel';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: any) {
-  const url = new URL(request.url);
-  const searchParams = new URLSearchParams(url.searchParams);
+	const url = new URL(request.url);
+	const searchParams = new URLSearchParams(url.searchParams);
 
-  const app_user_id = searchParams.get('app_id');
+	const app_user_id = searchParams.get('app_id');
 
-  await dbConnect();
-  const user = await UserModel.findOne({ app_user_id: app_user_id });
-  if (!user) {
-    return NextResponse.json(
-      {
-        link: `https://t.me/red_blue_game_bot?start=app_user_id=${app_user_id}`,
-        referals: [],
-      },
-      { status: 200 }
-    );
-  }
-  const referals = await UserModel.find({ parent_id: user.user_id });
-
-  return NextResponse.json(
-    {
-      link: `https://t.me/red_blue_game_bot?start=app_user_id=${app_user_id}`,
-      referals,
-    },
-    { status: 200 }
-  );
+	await dbConnect();
+	const user = await UserModel.findOne({ app_user_id: app_user_id });
+	if (!user) {
+		return NextResponse.json(
+			{
+				link: `https://t.me/red_blue_game_bot?start=app_user_id=${app_user_id}`,
+				referals: [],
+			},
+			{ status: 200 }
+		);
+	}
+	const referals = await UserModel.find({ parent_id: user.user_id });
+	const referalIds = referals.filter((ref) => ref.active).map((ref) => ref.app_user_id);
+	return NextResponse.json(
+		{
+			link: `https://t.me/red_blue_game_bot?start=app_user_id=${app_user_id}`,
+			referals: referalIds,
+		},
+		{ status: 200 }
+	);
 }
 
 export async function POST(request: any) {
-  const data = await request.json();
-  const { app_user_id } = data;
+	const data = await request.json();
+	const { app_user_id } = data;
 
-  return NextResponse.json(
-    {
-      link: `https://t.me/red_blue_game_bot?start=app_user_id=${app_user_id}`,
-    },
-    { status: 200 }
-  );
+	return NextResponse.json(
+		{
+			link: `https://t.me/red_blue_game_bot?start=app_user_id=${app_user_id}`,
+		},
+		{ status: 200 }
+	);
 }
